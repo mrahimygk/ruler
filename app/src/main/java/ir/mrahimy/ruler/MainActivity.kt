e5f2c8ded8c542a6c13bbb8abc5c2d532537533f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-15 01:58:16 +0430

    using ktx to get vmodel

--- app/src/main/java/ir/mrahimy/ruler/MainActivity.kt
+++ app/src/main/java/ir/mrahimy/ruler/MainActivity.kt
@@ -2,15 +2,27 @@ package ir.mrahimy.ruler
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import ir.mrahimy.ruler.databinding.ActivityMainBinding
+import ir.mrahimy.ruler.ktx.getMainActivityViewModel
 import ir.mrahimy.ruler.ktx.getRealDeviceSizeInPixels
 import ir.mrahimy.ruler.ktx.hideToolBr
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        viewModel = getMainActivityViewModel()
+
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply {
+            lifecycleOwner = this@MainActivity
+            vm = viewModel
+            executePendingBindings()
+        }
         hideToolBr()
 
         val displayInfo = getRealDeviceSizeInPixels()
