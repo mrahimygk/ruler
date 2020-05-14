2c386b0e5e5cb9199cf90604b4b5519efd34c950
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-15 00:16:47 +0430

    +data class to hold screen data

---
+++ app/src/main/java/ir/mrahimy/ruler/data/DisplayInfo.kt
@@ -0,0 +1,10 @@
+package ir.mrahimy.ruler.data
+
+data class DisplayInfo(
+    val wInPixel: Float,
+    val hInPixel: Float,
+    val xdpi: Float,
+    val ydpi: Float,
+    val wInInch: Float,
+    val hInInch: Float
+)
